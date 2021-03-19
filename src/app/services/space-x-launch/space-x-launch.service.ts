53e02efb5f53cf2a7232049ddf62e32c4f3144f0
geet365 <sangeeta4521@gmail.com>
2021-03-20 01:21:29 +0530

    spacexlaunch service spec added

--- src/app/services/space-x-launch/space-x-launch.service.ts
+++ src/app/services/space-x-launch/space-x-launch.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { forEach, toString } from 'lodash-es';
-import { environment } from '../../../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
 import { QueryParams, constructQueryParams } from 'src/app/helpers';
-import spaceX from './space-x.types';
 
 export type SpaceXQueryParams = { launch_success?: boolean, land_success?: boolean, launch_year?: number };
 
@@ -12,6 +14,7 @@ export type SpaceXQueryParams = { launch_success?: boolean, land_success?: boole
 })
 export class SpaceXLaunchService {
   private baseUrl = environment.spaceXLaunchApiBaseUrl;
+
   private defaultQueryParams = { limit: 100 };
 
   constructor(private http: HttpClient) {}
@@ -34,9 +37,31 @@ export class SpaceXLaunchService {
     return transformedQuery;
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+  /**
+   * Queries space-x launch api with query params
+   */
   queryAll(query: SpaceXQueryParams = {}) {
     const compatibleQuery = this.transformToCompatibleQuery(query);
 
-    return this.http.get<spaceX.Launch>(this.baseUrl, { params: this.getParams(compatibleQuery) });
+    return this.http.get<Record<string, any>[]>(this.baseUrl, { params: this.getParams(compatibleQuery) }).pipe(
+      retry(3), // retry up to 3 times
+      catchError(this.handleError) // handle the error
+    );
   }
 }
