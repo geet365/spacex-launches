06d62ec0bf3b0dab1117563ce64fc92142491390
geet365 <sangeeta4521@gmail.com>
2021-03-20 01:52:28 +0530

    lint fix

--- src/app/helpers/contructQueryParams.ts
+++ src/app/helpers/contructQueryParams.ts
@@ -5,7 +5,7 @@ export type QueryParams = { [param: string]: string | string[] };
 /**
  * It accepts query params object and returns HttpParams instance
  */
-export function constructQueryParams(queryParams: QueryParams) {
+export function constructQueryParams(queryParams: QueryParams): HttpParams {
   const params = new HttpParams();
   return params.appendAll(queryParams);
 }
